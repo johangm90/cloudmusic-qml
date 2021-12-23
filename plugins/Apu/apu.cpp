45fed3a2d09e8f3c9a4f7bcbaeeba599a796d14f
Matteo <mattbel10@hotmail.com>
2021-12-23 17:09:10 +0100

    added function to change transferred file name

--- plugins/Apu/apu.cpp
+++ plugins/Apu/apu.cpp
@@ -1,11 +1,31 @@
 #include <QDebug>
+#include <QString>
+#include <QProcess>
 
 #include "apu.h"
+
+using namespace std;
 
 Apu::Apu() {
 
 }
 
-void Apu::speak() {
-    qDebug() << "hello world!";
+void Apu::changeFileName(QString urlFile, QString fileDir,QString nameSong) {
+  array<char, 128> buffer;
+  QString QSresult;
+  string result;
+  string command = "mv -f " + urlFile.toStdString() + " " + fileDir.toStdString() + nameSong.toStdString() + ".mp3";
+  QString commandStr = QString::fromStdString(command);
+  FILE* pipe = popen(command.c_str(), "r");
+  if (!pipe) {
+    qDebug() << "Couldn't start command -" << commandStr << "-";
+  } else {
+    qDebug() << "Command -" << commandStr << "- executed with success.";
+  }
+  while (fgets(buffer.data(), 128, pipe) != NULL) {
+    result += buffer.data();
+    QSresult = QString::fromStdString(result);
+    QSresult = QSresult.trimmed();
+  }
+  qDebug() << QSresult;
 }
